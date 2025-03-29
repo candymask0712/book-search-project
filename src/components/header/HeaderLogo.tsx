c3efe1359c2f65bec849b6e30cae7a735a6043d2
candymask0712 <candymask0712@gmail.com>
2025-03-29 15:46:00 +0900

    chore: 새로운 폰트 크기 추가로 인한 기존 코드 수정

--- src/components/header/HeaderLogo.tsx
+++ src/components/header/HeaderLogo.tsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
-import { DEFAULT_LINK_HREF } from '../../constants/links';
+import { DEFAULT_LINK_HREF } from '../../constants/nav';
 import COMMON from '../../constants/common';
 
 const HeaderLogo: React.FC = () => {
   return (
     <Link to={DEFAULT_LINK_HREF}>
-      <h1 className='text-title1'>{COMMON.TITLE}</h1>
+      <h1 className="text-title1">{COMMON.TITLE}</h1>
     </Link>
   );
 };
