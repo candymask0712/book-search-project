f9cefe7ada52eadc2a3afa1d26bfd656639375d8
candymask0712 <candymask0712@gmail.com>
2025-03-31 01:09:27 +0900

    fix: 시안에 맞게 헤더 css 위치 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,11 +6,11 @@ import HeaderLogo from './HeaderLogo';
 const Header: React.FC = () => {
   return (
     <header className="w-full bg-palette-white fixed top-0 z-30">
-      <div className="max-w-screen-xl mx-auto h-16 flex items-center">
-        <div className="flex-[4]">
+      <div className="flex h-16 items-center">
+        <div className="flex basis-1/4 justify-center">
           <HeaderLogo />
         </div>
-        <div className="flex-[6]">
+        <div className="basis-3/4 flex justify-start pl-80">
           <HeaderNav />
         </div>
       </div>
