e681f452b2eaeca24445cd100ad2950d460a968c
candymask0712 <candymask0712@gmail.com>
2025-03-31 01:12:17 +0900

    style: 메인 레이아웃 css 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from './components/header/Header';
 
 export default function App() {
   return (
-    <div className="bg-palette-white h-screen">
+    <div className="bg-palette-white">
       <Header />
       <main className="pt-16 max-w-[960px] mx-auto mt-14">
         <Outlet />
